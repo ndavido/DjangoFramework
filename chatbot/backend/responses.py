86f657272174152ea42c733c85a8be945544ba87
ndavido <D00239107@student.dkit.ie>
2023-03-23 13:28:20 +0200

    :sparkles: Query Parameters

---
+++ chatbot/backend/responses.py
@@ -0,0 +1,7 @@
+def bot_response(message):
+    if message == "hello": 
+        return "Hello there human being!"
+    elif message == "goodbye":
+        return "Short circuit executed!"
+    else:
+        return "Cannot comprehend"
--- chatbot/backend/views.py
+++ chatbot/backend/views.py
@@ -6,9 +6,13 @@ def backend(request, slug=None):
     return HttpResponse("<p>Hello from the backend-side</p>")
 
 import json
+from .responses import bot_response
 
 def get_chat_response(request, slug=None):
+    data = request.GET
+    message = data.get("message")
+    
     response = {
-        "message": "Hello from chatbot"
+        "message": bot_response(message)
     }
     return HttpResponse(json.dumps(response))
