4b8667c7aa705e81a11fb88181230f156795a575
ndavido <D00239107@student.dkit.ie>
2023-03-23 17:13:22 +0200

    :sparkles: Wikipedia search implemented

--- chatbot/backend/responses.py
+++ chatbot/backend/responses.py
@@ -10,5 +10,16 @@ def bot_response(message):
         url = f"https://picsum.photos/500/500/?id={id}"
         generated_image = f"<img src='{url}' width='500' height='500'>"
         return generated_image
+    elif message.startswith("Tell me about"):
+        # list = message.split()
+        # for i in range(3):
+        #     list.pop(0)
+        #     print(list)
+       
+        # print(list)
+        # search = " ".join(list)
+        search = message.replace("Tell me about ","")
+        url = f"<a href='https://en.wikipedia.org/wiki/{search}' target='_blank'>https://en.wikipedia.org/wiki/{search}</a>"
+        return f"Here is what I found: {url}"
     else:
         return "Cannot comprehend"
