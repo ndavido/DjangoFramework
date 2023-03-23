0b7e87f36553576bbfcefa7a50be54b87ecfe777
ndavido <D00239107@student.dkit.ie>
2023-03-23 16:43:13 +0200

    :sparkles: Random Images generated

--- chatbot/backend/responses.py
+++ chatbot/backend/responses.py
@@ -1,7 +1,14 @@
+import random
+
 def bot_response(message):
     if message == "hello": 
         return "Hello there human being!"
     elif message == "goodbye":
         return "Short circuit executed!"
+    elif message == "Gimme image":
+        id = str(random.randint(1, 1000))
+        url = f"https://picsum.photos/500/500/?id={id}"
+        generated_image = f"<img src='{url}' width='500' height='500'>"
+        return generated_image
     else:
         return "Cannot comprehend"
