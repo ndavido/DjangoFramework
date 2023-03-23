749f70995f4a5006ad8d94ad2d0e37845edc152e
ndavido <D00239107@student.dkit.ie>
2023-03-23 17:41:43 +0200

    :sparkles: Implemented repeat message

--- chatbot/backend/responses.py
+++ chatbot/backend/responses.py
@@ -1,25 +1,37 @@
 import random
 
+last_message = ""
+
 def bot_response(message):
-    if message == "hello": 
+    global last_message
+    if message == last_message:
+        return "STOP REPEATING YOURSELF!"
+    elif message == "hello":
+        last_message = message
         return "Hello there human being!"
     elif message == "goodbye":
+        last_message = message
         return "Short circuit executed!"
     elif message == "Gimme image":
+        last_message = message
         id = str(random.randint(1, 1000))
         url = f"https://picsum.photos/500/500/?id={id}"
         generated_image = f"<img src='{url}' width='500' height='500'>"
         return generated_image
     elif message.startswith("Tell me about"):
-        # list = message.split()
-        # for i in range(3):
-        #     list.pop(0)
-        #     print(list)
-       
-        # print(list)
-        # search = " ".join(list)
+        last_message = message
         search = message.replace("Tell me about ","")
         url = f"<a href='https://en.wikipedia.org/wiki/{search}' target='_blank'>https://en.wikipedia.org/wiki/{search}</a>"
         return f"Here is what I found: {url}"
+    elif message == "Help":
+        last_message = message
+        commands = '''
+            Yes
+        '''
+        return commands
     else:
-        return "Cannot comprehend"
+        last_message = message
+        return "Cannot comprehend, type 'Help' to find out what I know :)"
+        
+    
+    
